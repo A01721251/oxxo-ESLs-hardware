e013a8035ef77bbf586d2418f3becd9e172c4cb4
Tuto <a01721251@tec.mx>
2024-06-04 17:54:15 -0600

    Same

--- display.py
+++ display.py
@@ -30,8 +30,7 @@ def update_display_design1(product_name, price):
         # Load the OXXO logo and prepare it for drawing
         logo_path = os.path.join(imgdir, 'oxxo.png')
         if os.path.exists(logo_path):
-            logo = Image.open(logo_path)
-            logo = logo.convert("RGBA")
+            logo = Image.open(logo_path).convert("RGBA")
             logo_w, logo_h = logo.size
             max_size = (epd.height - 20, epd.width - 20)  # Leaving some margin
             logo.thumbnail(max_size, Image.ANTIALIAS)
@@ -40,21 +39,21 @@ def update_display_design1(product_name, price):
             logo_x = (epd.height - logo.size[0]) // 2
             logo_y = (epd.width - logo.size[1]) // 2
 
-            # Split the logo into its red and black parts
-            red_image = Image.new("1", logo.size, 255)
-            black_image = Image.new("1", logo.size, 255)
+            # Prepare masks for black and red
+            red_mask = Image.new("1", logo.size, 255)
+            black_mask = Image.new("1", logo.size, 255)
 
             for y in range(logo.size[1]):
                 for x in range(logo.size[0]):
                     r, g, b, a = logo.getpixel((x, y))
-                    if r > 200 and g < 50 and b < 50:  # Red parts
-                        red_image.putpixel((x, y), 0)  # Red ink
-                    elif r < 50 and g < 50 and b < 50:  # Black parts
-                        black_image.putpixel((x, y), 0)  # Black ink
+                    if r > 200 and g < 50 and b < 50 and a > 0:  # Red parts
+                        red_mask.putpixel((x, y), 0)
+                    elif r < 50 and g < 50 and b < 50 and a > 0:  # Black parts
+                        black_mask.putpixel((x, y), 0)
 
             # Draw the logo parts
-            Rimage.paste(red_image, (logo_x, logo_y), mask=red_image)
-            Himage.paste(black_image, (logo_x, logo_y), mask=black_image)
+            Rimage.paste(logo, (logo_x, logo_y), mask=red_mask)
+            Himage.paste(logo, (logo_x, logo_y), mask=black_mask)
 
         # Draw product name in black
         draw_black.text((10, 10), product_name, font=font24, fill=0)  # 0: black
